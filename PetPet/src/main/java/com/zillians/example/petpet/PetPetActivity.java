9c1babc0bf5dc6e1daf425b60459427768a93979
giggle <giggle@zillians.com>
2013-09-30 15:23:30 +0800

    pass local description to the other peer

--- PetPet/src/main/java/com/zillians/example/petpet/PetPetActivity.java
+++ PetPet/src/main/java/com/zillians/example/petpet/PetPetActivity.java
@@ -6,6 +6,8 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.webrtc.DataChannel;
 import org.webrtc.IceCandidate;
 import org.webrtc.MediaConstraints;
@@ -17,9 +19,14 @@ import org.webrtc.SessionDescription;
 import org.webrtc.StatsObserver;
 import org.webrtc.StatsReport;
 
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class PetPetActivity extends Activity {
     private static final String TAG = "PetPetActivity";
@@ -38,6 +45,9 @@ public class PetPetActivity extends Activity {
                     SessionDescription sdp = new SessionDescription(
                             sessionDescription.type, SdpHelper.preferISAC(sessionDescription.description));
                     mPeerConnection.setLocalDescription(MySdpObserver.this, sdp);
+
+                    // Pass my description to the other peer
+                    sendSdp(sessionDescription);
                 }
             });
         }
@@ -98,13 +108,18 @@ public class PetPetActivity extends Activity {
         }
 
         @Override
-        public void onIceCandidate(IceCandidate iceCandidate) {
-            Log.d(TAG, "PeerConnectionObserver: onIceCandidate");
+        public void onIceCandidate(final IceCandidate iceCandidate) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.d(TAG, "PeerConnectionObserver: onIceCandidate");
 
-            Log.d(TAG, " ---- sdp = " + iceCandidate.sdp);
-            Log.d(TAG, " ==== sdpMid = " + iceCandidate.sdpMid);
-            Log.d(TAG, " ==== sdp line index = " + iceCandidate.sdpMLineIndex);
-            mQueuedRemoteCandidates.add(iceCandidate);
+                    Log.d(TAG, " ---- sdp = " + iceCandidate.sdp);
+                    Log.d(TAG, " ==== sdpMid = " + iceCandidate.sdpMid);
+                    Log.d(TAG, " ==== sdp line index = " + iceCandidate.sdpMLineIndex);
+                    mQueuedRemoteCandidates.add(iceCandidate);
+                }
+            });
         }
 
         @Override
@@ -129,12 +144,54 @@ public class PetPetActivity extends Activity {
     }
 
 
+    private static final String STUN_SERVER = "stun:stun.l.google.com:19302";
+    private static final String SIGNALING_SERVER = "https://apprtc.appspot.com/message?r=6666&u=80794597";
+
     private PeerConnectionFactory mPeerConnectionFactory;
     private PeerConnection mPeerConnection;
     private MediaConstraints mMediaConstraints;
     private LinkedList<IceCandidate> mQueuedRemoteCandidates = new LinkedList<IceCandidate>();
 
     private Handler mHandler;
+    private ExecutorService mExecutors = Executors.newSingleThreadExecutor();
+
+
+    private static void jsonPut(JSONObject json, String key, Object value) {
+        try {
+            json.put(key, value);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void sendSdp(final SessionDescription sdp) {
+        Log.d(TAG, "Send sdp to the other peer");
+        JSONObject json = new JSONObject();
+        jsonPut(json, "type", sdp.type.canonicalForm());
+        jsonPut(json, "sdp", sdp.description);
+        sendMessage(json);
+    }
+
+    private void sendMessage(final JSONObject jsonObject) {
+        mExecutors.submit(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URLConnection connection = null;
+                    connection = new URL(SIGNALING_SERVER).openConnection();
+                    connection.setDoOutput(true);
+                    connection.getOutputStream().write(jsonObject.toString().getBytes("UTF-8"));
+                    if (!connection.getHeaderField(null).startsWith("HTTP/1.1 200 ")) {
+                        throw new IOException(
+                                "Non-200 response to POST: " + connection.getHeaderField(null) +
+                                        " for msg: " + jsonObject.toString());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -175,7 +232,7 @@ public class PetPetActivity extends Activity {
 
         // Hard code ice servers
         List<PeerConnection.IceServer> iceServers = new ArrayList<PeerConnection.IceServer>();
-        iceServers.add(new PeerConnection.IceServer("stun:stun.l.google.com:19302", "", ""));
+        iceServers.add(new PeerConnection.IceServer(STUN_SERVER, "", ""));
 
         mPeerConnection = mPeerConnectionFactory.createPeerConnection(iceServers, new MediaConstraints(), new MyPeerConnectionObserver());
 
