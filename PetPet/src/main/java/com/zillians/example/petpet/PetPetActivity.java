aaffe36730984883e91bfc08bed813ce29632ab3
giggle <giggle@zillians.com>
2013-09-30 16:40:49 +0800

    refactor the codes to seperate signal process

--- PetPet/src/main/java/com/zillians/example/petpet/PetPetActivity.java
+++ PetPet/src/main/java/com/zillians/example/petpet/PetPetActivity.java
@@ -6,8 +6,6 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.webrtc.DataChannel;
 import org.webrtc.IceCandidate;
 import org.webrtc.MediaConstraints;
@@ -19,14 +17,9 @@ import org.webrtc.SessionDescription;
 import org.webrtc.StatsObserver;
 import org.webrtc.StatsReport;
 
-import java.io.IOException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class PetPetActivity extends Activity {
     private static final String TAG = "PetPetActivity";
@@ -47,7 +40,7 @@ public class PetPetActivity extends Activity {
                     mPeerConnection.setLocalDescription(MySdpObserver.this, sdp);
 
                     // Pass my description to the other peer
-                    sendSdp(sessionDescription);
+                    mSignalService.sendSdp(sessionDescription);
                 }
             });
         }
@@ -117,7 +110,7 @@ public class PetPetActivity extends Activity {
                     Log.d(TAG, " ---- sdp = " + iceCandidate.sdp);
                     Log.d(TAG, " ==== sdpMid = " + iceCandidate.sdpMid);
                     Log.d(TAG, " ==== sdp line index = " + iceCandidate.sdpMLineIndex);
-                    sendIceCandidate(iceCandidate);
+                    mSignalService.sendIceCandidate(iceCandidate);
                 }
             });
         }
@@ -144,7 +137,6 @@ public class PetPetActivity extends Activity {
     }
 
     private static final String STUN_SERVER = "stun:stun.l.google.com:19302";
-    private static final String SIGNALING_SERVER = "https://apprtc.appspot.com/message?r=6666&u=80794597";
 
     private PeerConnectionFactory mPeerConnectionFactory;
     private PeerConnection mPeerConnection;
@@ -152,55 +144,7 @@ public class PetPetActivity extends Activity {
     private LinkedList<IceCandidate> mQueuedRemoteCandidates = new LinkedList<IceCandidate>();
 
     private Handler mHandler;
-    private ExecutorService mExecutors = Executors.newSingleThreadExecutor();
-
-
-    private static void jsonPut(JSONObject json, String key, Object value) {
-        try {
-            json.put(key, value);
-        } catch (JSONException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void sendIceCandidate(IceCandidate iceCandidate) {
-        JSONObject json = new JSONObject();
-        jsonPut(json, "type", "candidate");
-        jsonPut(json, "label", iceCandidate.sdpMLineIndex);
-        jsonPut(json, "id", iceCandidate.sdpMid);
-        jsonPut(json, "candidate", iceCandidate.sdp);
-        sendMessage(json);
-    }
-
-    private void sendSdp(final SessionDescription sdp) {
-        Log.d(TAG, "Send sdp to the other peer");
-        JSONObject json = new JSONObject();
-        jsonPut(json, "type", sdp.type.canonicalForm());
-        jsonPut(json, "sdp", sdp.description);
-        sendMessage(json);
-    }
-
-    private void sendMessage(final JSONObject jsonObject) {
-        mExecutors.submit(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    URLConnection connection = null;
-                    connection = new URL(SIGNALING_SERVER).openConnection();
-                    connection.setDoOutput(true);
-                    connection.getOutputStream().write(jsonObject.toString().getBytes("UTF-8"));
-                    Log.d(TAG, "SendMessage Response: " + connection.getHeaderField(null).toString());
-                    if (!connection.getHeaderField(null).startsWith("HTTP/1.1 200 ")) {
-                        throw new IOException(
-                                "Non-200 response to POST: " + connection.getHeaderField(null) +
-                                        " for msg: " + jsonObject.toString());
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
+    private SignalService mSignalService = new SignalService();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
