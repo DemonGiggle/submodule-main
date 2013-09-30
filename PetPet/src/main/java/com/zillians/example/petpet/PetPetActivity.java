33afe10c9150ed571334fcde3049db652af02f1b
giggle <giggle@zillians.com>
2013-09-30 11:53:17 +0800

    first commit, at least we have ice candidates now

---
+++ PetPet/src/main/java/com/zillians/example/petpet/PetPetActivity.java
@@ -0,0 +1,195 @@
+package com.zillians.example.petpet;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.Menu;
+
+import org.webrtc.DataChannel;
+import org.webrtc.IceCandidate;
+import org.webrtc.MediaConstraints;
+import org.webrtc.MediaStream;
+import org.webrtc.PeerConnection;
+import org.webrtc.PeerConnectionFactory;
+import org.webrtc.SdpObserver;
+import org.webrtc.SessionDescription;
+import org.webrtc.StatsObserver;
+import org.webrtc.StatsReport;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class PetPetActivity extends Activity {
+    private static final String TAG = "PetPetActivity";
+
+    class MySdpObserver implements SdpObserver {
+
+        @Override
+        public void onCreateSuccess(final SessionDescription sessionDescription) {
+            Log.d(TAG, "SdpObserver: onCreateSuccess");
+            Log.d(TAG, "Sending: " + sessionDescription.type);
+//            Log.d(TAG, "Description: " + sessionDescription.description);
+
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    Log.d(TAG, "SdpObserver: Set local description");
+                    SessionDescription sdp = new SessionDescription(
+                            sessionDescription.type, SdpHelper.preferISAC(sessionDescription.description));
+                    mPeerConnection.setLocalDescription(MySdpObserver.this, sdp);
+                }
+            });
+        }
+
+        private void drainRemoteCandidates() {
+            for (IceCandidate candidate : mQueuedRemoteCandidates) {
+                mPeerConnection.addIceCandidate(candidate);
+            }
+            mQueuedRemoteCandidates.clear();
+        }
+
+        @Override
+        public void onSetSuccess() {
+            Log.d(TAG, "SdpObserver: onSetSuccess");
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (mPeerConnection.getLocalDescription() == null) {
+                        // We just set the remote offer, time to create our answer.
+                        Log.d(TAG, "SdpObserver: Creating answer");
+                        mPeerConnection.createAnswer(MySdpObserver.this, mMediaConstraints);
+                    } else {
+                        Log.d(TAG, "SdpObserver: drain remote candidates");
+                        // Sent our answer and set it as local description; drain
+                        // candidates.
+                        drainRemoteCandidates();
+                    }
+                }
+            });
+        }
+
+        @Override
+        public void onCreateFailure(String s) {
+            Log.d(TAG, "SdpObserver: onCreateFailure");
+        }
+
+        @Override
+        public void onSetFailure(String s) {
+            Log.d(TAG, "SdpObserver: onSetFailure");
+        }
+    }
+
+    class MyPeerConnectionObserver implements PeerConnection.Observer {
+
+        @Override
+        public void onSignalingChange(PeerConnection.SignalingState signalingState) {
+            Log.d(TAG, "PeerConnectionObserver: onSignalingChange");
+        }
+
+        @Override
+        public void onIceConnectionChange(PeerConnection.IceConnectionState iceConnectionState) {
+            Log.d(TAG, "PeerConnectionObserver: onIceConnectionChange");
+        }
+
+        @Override
+        public void onIceGatheringChange(PeerConnection.IceGatheringState iceGatheringState) {
+            Log.d(TAG, "PeerConnectionObserver: onIceGatheringChange");
+        }
+
+        @Override
+        public void onIceCandidate(IceCandidate iceCandidate) {
+            Log.d(TAG, "PeerConnectionObserver: onIceCandidate");
+
+            Log.d(TAG, " ---- sdp = " + iceCandidate.sdp);
+            Log.d(TAG, " ==== sdpMid = " + iceCandidate.sdpMid);
+            Log.d(TAG, " ==== sdp line index = " + iceCandidate.sdpMLineIndex);
+            mQueuedRemoteCandidates.add(iceCandidate);
+        }
+
+        @Override
+        public void onError() {
+            Log.d(TAG, "PeerConnectionObserver: onError");
+        }
+
+        @Override
+        public void onAddStream(MediaStream mediaStream) {
+            Log.d(TAG, "PeerConnectionObserver: onAddStream");
+        }
+
+        @Override
+        public void onRemoveStream(MediaStream mediaStream) {
+            Log.d(TAG, "PeerConnectionObserver: onRemoveStream");
+        }
+
+        @Override
+        public void onDataChannel(DataChannel dataChannel) {
+            Log.d(TAG, "PeerConnectionObserver: onDataChannel");
+        }
+    }
+
+
+    private PeerConnectionFactory mPeerConnectionFactory;
+    private PeerConnection mPeerConnection;
+    private MediaConstraints mMediaConstraints;
+    private LinkedList<IceCandidate> mQueuedRemoteCandidates = new LinkedList<IceCandidate>();
+
+    private Handler mHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private Runnable checkPeerState = new Runnable() {
+        @Override
+        public void run() {
+
+            boolean success = mPeerConnection.getStats(new StatsObserver() {
+                public void onComplete(StatsReport[] reports) {
+                    for (StatsReport report : reports) {
+                        Log.d(TAG, "Stats: " + report.toString());
+                    }
+                    mHandler.postDelayed(checkPeerState, 10000);
+                }
+            }, null);
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mHandler = new Handler();
+
+        PeerConnectionFactory.initializeAndroidGlobals(this);
+        mPeerConnectionFactory = new PeerConnectionFactory();
+
+        // Specify what data to transfer
+        mMediaConstraints = new MediaConstraints();
+//        mMediaConstraints.mandatory.add(new MediaConstraints.KeyValuePair(
+//                "OfferToReceiveAudio", "true"));
+//        mMediaConstraints.mandatory.add(new MediaConstraints.KeyValuePair(
+//                "OfferToReceiveVideo", "true"));
+
+        // Hard code ice servers
+        List<PeerConnection.IceServer> iceServers = new ArrayList<PeerConnection.IceServer>();
+        iceServers.add(new PeerConnection.IceServer("stun:stun.l.google.com:19302", "", ""));
+
+        mPeerConnection = mPeerConnectionFactory.createPeerConnection(iceServers, new MediaConstraints(), new MyPeerConnectionObserver());
+        mHandler.postDelayed(checkPeerState, 10000);
+
+        mPeerConnection.createOffer(new MySdpObserver(), mMediaConstraints);
+
+        Log.d(TAG, "Peer Connection created");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.pet_pet, menu);
+        return true;
+    }
+    
+}
---
+++ PetPet/src/main/java/com/zillians/example/petpet/SdpHelper.java
@@ -0,0 +1,62 @@
+package com.zillians.example.petpet;
+
+import android.util.Log;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by giggle on 9/30/13.
+ */
+public class SdpHelper {
+    private static final String TAG = "PetPet.SdpHelper";
+
+    // Mangle SDP to prefer ISAC/16000 over any other audio codec.
+    public static String preferISAC(String sdpDescription) {
+        String[] lines = sdpDescription.split("\n");
+        int mLineIndex = -1;
+        String isac16kRtpMap = null;
+        Pattern isac16kPattern =
+                Pattern.compile("^a=rtpmap:(\\d+) ISAC/16000[\r]?$");
+        for (int i = 0;
+             (i < lines.length) && (mLineIndex == -1 || isac16kRtpMap == null);
+             ++i) {
+            if (lines[i].startsWith("m=audio ")) {
+                mLineIndex = i;
+                continue;
+            }
+            Matcher isac16kMatcher = isac16kPattern.matcher(lines[i]);
+            if (isac16kMatcher.matches()) {
+                isac16kRtpMap = isac16kMatcher.group(1);
+                continue;
+            }
+        }
+        if (mLineIndex == -1) {
+            Log.d(TAG, "No m=audio line, so can't prefer iSAC");
+            return sdpDescription;
+        }
+        if (isac16kRtpMap == null) {
+            Log.d(TAG, "No ISAC/16000 line, so can't prefer iSAC");
+            return sdpDescription;
+        }
+        String[] origMLineParts = lines[mLineIndex].split(" ");
+        StringBuilder newMLine = new StringBuilder();
+        int origPartIndex = 0;
+        // Format is: m=<media> <port> <proto> <fmt> ...
+        newMLine.append(origMLineParts[origPartIndex++]).append(" ");
+        newMLine.append(origMLineParts[origPartIndex++]).append(" ");
+        newMLine.append(origMLineParts[origPartIndex++]).append(" ");
+        newMLine.append(isac16kRtpMap).append(" ");
+        for (; origPartIndex < origMLineParts.length; ++origPartIndex) {
+            if (!origMLineParts[origPartIndex].equals(isac16kRtpMap)) {
+                newMLine.append(origMLineParts[origPartIndex]).append(" ");
+            }
+        }
+        lines[mLineIndex] = newMLine.toString();
+        StringBuilder newSdpDescription = new StringBuilder();
+        for (String line : lines) {
+            newSdpDescription.append(line).append("\n");
+        }
+        return newSdpDescription.toString();
+    }
+}
